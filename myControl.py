7a1f0d3eed1ebcad779f3b92d49b2e2302cfffb4
ewhale <ewhale7@gmail.com>
2021-06-29 14:07:00 -0500

    Update myControl.py
    Moved NFC read to load game function here from myButtons.py for a more fluid operation.

--- myControl.py
+++ myControl.py
@@ -1,4 +1,6 @@
 import os, subprocess
+from myNFC import read
+from myProcesses import runGame
 
 ## Starts fan control
 os.system('pkill -9 -f myFan.py')
@@ -7,3 +9,9 @@ subprocess.Popen('python /home/pi/scripts/myControl/myFan.py&', shell=True)
 ## Starts button control
 os.system('pkill -9 -f myButtons.py')
 subprocess.Popen('python /home/pi/scripts/myControl/myButtons.py&', shell=True)
+
+## Check for NFC tag and boot if present
+response = read()
+if response.type == 'success':
+    message = response.data
+    runGame(message.records[0].value, message.records[1].value, 'nfc')
